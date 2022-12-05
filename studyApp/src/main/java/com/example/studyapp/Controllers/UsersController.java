b07d9dfc83120c51185c280babf3ab3e78625be3
Muchacho <0elephant123@gmail.com>
2022-12-05 18:12:47 +0300

    :)

--- studyApp/src/main/java/com/example/studyapp/Controllers/UsersController.java
+++ studyApp/src/main/java/com/example/studyapp/Controllers/UsersController.java
@@ -7,6 +7,7 @@ import com.example.studyapp.DB.Repo.GradeRepo;
 import com.example.studyapp.DB.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,12 +32,21 @@ public class UsersController {
 
 
     @PostMapping("/findUsers")
-    public String findUsers(@RequestParam Integer id, Map<String, Object> model){
-        List<GradeUntity> gradesList = gradeRepo.findByStudent(id);
-        //UserEntity user = userRepo.findByLogin(login);
-       // List<UserEntity> users = new ArrayList<>();
-       // users.add(user);
-        model.put("GradesList", gradesList);
+    public String findUsers(UserEntity user, Model model){
+        List<GradeUntity> gradesList = gradeRepo.findByStudent(user.getId());
+        if(user.getId() == null){
+            System.out.println("no id");
+            model.addAttribute("error", "Wrong id");
+            model.addAttribute("GradesList",gradeRepo.findAll());
+            return "users";
+        }
+        else if(gradesList.isEmpty()){
+            System.out.println("user not exist");
+            model.addAttribute("error", "Student not exist");
+            model.addAttribute("GradesList",gradeRepo.findAll());
+            return "users";
+        }
+        model.addAttribute("GradesList", gradesList);
         return "users";
     }
 }
